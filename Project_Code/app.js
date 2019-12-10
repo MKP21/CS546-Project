27544e40ab78957c75dc577064f3b4364b75ea4c
Meet <meetknpatel2@gmail.com>
2019-12-09 19:17:57 -0500

    fifth commit

--- Project_Code/app.js
+++ Project_Code/app.js
@@ -6,9 +6,16 @@ const bcrypt = require('bcryptjs');
 const bodyparser = require('body-parser');
 const static = express.static(__dirname + '/public');
 const configRoutes=require('./routes');
+const roomFunctions=require('./data/room');
+const userFunctions=require('./data/user');
+
+// socket.io session and cookie session
+const sharedsession = require("express-socket.io-session");
+const cookie = require('cookie');
 
 // We create our express instance:
 const app = express();
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -27,8 +34,6 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
-
-
 
 // 1.Middleware :runs every time
 app.use('/',function(req, res, next) {
@@ -55,7 +60,7 @@ app.get('/user', async (req, res, next) => {
 });
 
 // 2.Middleware for room route
-app.get('/room', async (req, res, next) => {
+app.use('/room', async (req, res, next) => {
   let sess = req.session; 
   if(!sess.uMail){
     //If a user is not logged in
@@ -70,6 +75,33 @@ app.get('/room', async (req, res, next) => {
 configRoutes(app);
 
 // We can now navigate to localhost:3000
-app.listen(3000, function() {
+server =app.listen(3000, function() {
   console.log('Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it');
+});
+
+const io=require("socket.io")(server);
+
+// creating a shared cookie based off on express session
+// io.use(sharedsession(session, {
+//   autoSave:true
+// }));
+
+io.on('connection', async function(socket){
+  console.log('a user connected');
+  //var userObj = await userFunctions.getUserByEmail(req.session.uMail);
+  
+
+  // this will be called when send is pressed
+  socket.on('chat message', function(msg){
+
+    var ses = cookie.parse(socket.handshake.headers.cookie);
+    var exps = cookie.parse(ses.AuthCookie);
+    
+    console.log('message: ' + msg);
+    
+    //userObj.firstName + " " + userObj.lastName + ": " +
+    // This will emit the event to all connected sockets
+    io.emit('chat message',msg); 
+});
+
 });
